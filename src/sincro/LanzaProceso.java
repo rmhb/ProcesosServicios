ffb04470c80f7267f68551e58e7dc0f6983e9d91
Rosa <rosa.hb84@gmail.com>
2022-10-05 11:24:25 +0200

    Sincro

---
+++ src/sincro/LanzaProceso.java
@@ -0,0 +1,40 @@
+
+package sincro;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ *
+ * @author NASA
+ */
+public class LanzaProceso {
+     public static void main(String[] args) {
+        //    1. Ejecución sin errores, pasar como argumento para el main desde la interfaz run-> Set project configuration -> arguments : notepad  también se puede probar con cmd /c
+        //    2. Ejecución programa no existe: pasar como pargumento p_noExiste
+        //    3. Ejecución proceso que termina en codigo error, pasar como argumento: cmd /c/nas dir
+
+         if( args.length <= 0 ){
+            System.out.println("Para que funcione indica comando a ejecutar: ");
+            System.exit(1);
+        }
+        ProcessBuilder pb = new ProcessBuilder(args);
+        pb.inheritIO();
+        try{
+            Process p = pb.start();
+            int codigo = p.waitFor();
+            System.out.println("La ejecución de "+Arrays.toString(args)+
+                    " devuelve código "+codigo+ " "+ (codigo == 0 ? " Accion completada correctamente " : " ERROR "));
+        }catch(IOException e){
+            System.out.println("Error durante la ejecución del proceso");
+            System.out.println("Información Detallada");
+            System.out.println("___________________________________________");
+            e.printStackTrace();
+            System.out.println("___________________________________________");
+            System.exit(2);
+        }catch(InterruptedException e){
+            System.err.println("Proceso Interrumpido");
+            System.exit(3);
+        }
+    }
+}
---
+++ src/sincro/ProcesoSecundario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sincro;
+
+/**
+ *
+ * @author NASA
+ */
+public class ProcesoSecundario {
+    public static void main(String[] args){
+        System.out.println("Proceso secundario...");
+        System.exit(103);
+    }
+}
